dcc6fa7269021c375620f3f8cc561e75266e7bb7
breno duarte <brenoo.duarte@hotmail.com>
2022-08-22 17:13:43 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UniLibrary
+free digital library
---
+++ src/test/java/br/com/UniLibrary/UniLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.UniLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
